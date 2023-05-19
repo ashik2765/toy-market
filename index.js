1f7643add57bea3d00eea85e7c22792b51dff065
Ashikur Rahman <ashikurrahman2765@gmail.com>
2023-05-20 00:03:25 +0600

    fixed error

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
+
 //middlewares
+// const corsConfig = {
+//     origin: '',
+//     credentials: true,
+//     methods: ['GET', 'POST', 'PUT', 'DELETE']
+// }
+// app.use(cors(corsConfig))
+// app.options("", cors(corsConfig))
 app.use(cors());
 app.use(express.json());
 
@@ -26,6 +34,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
     
 
     const toyCollection = client.db('toyShop').collection('toys');
