8b1f7d8d431d518eadbd63578edfbad88cf426e5
Ashikur Rahman <ashikurrahman2765@gmail.com>
2023-05-20 13:43:27 +0600

    make api for sub-category

--- index.js
+++ index.js
@@ -35,22 +35,33 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
-    
+
 
     const toyCollection = client.db('toyShop').collection('toys');
 
-    app.post('/postToy', async(req,res)=>{
-        const body = req.body;
-        const result = await toyCollection.insertOne(body);
-        res.send(result);
+    app.post('/postToy', async (req, res) => {
+      const body = req.body;
+      const result = await toyCollection.insertOne(body);
+      res.send(result);
     })
 
-    app.get('/toys',async(req,res)=>{
-        const cursor = toyCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
+    app.get('/toys', async (req, res) => {
+      const cursor = toyCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
     })
-    
+
+    app.get("/toy/:text", async (req, res) => {
+      const text = req.params?.text
+      const result = await toyCollection.find({
+        category: req.params?.text
+      }).toArray();
+      return res.send(result);
+    });
+
+
+   
+
     //get single data from database
     app.get("/toys/:id", async (req, res) => {
       console.log(req.params.id);
@@ -77,9 +88,9 @@ run().catch(console.dir);
 
 //Basic setups
 app.get('/', (req, res) => {
-    res.send('toy shop server is running')
+  res.send('toy shop server is running')
 })
 
 app.listen(port, () => {
-    console.log(`Toy shop server is running on port:${port}`)
+  console.log(`Toy shop server is running on port:${port}`)
 })
